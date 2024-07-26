b5a774fba31a712d511572c08cfd5e310f5e3ac9
Talha Ansari <140020341+talha-ansarii@users.noreply.github.com>
2024-07-26 07:59:31 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import Banner from "../components/Banner";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import ProductList from "@/components/ProductList";
 
-export default function Home() {
-  return (
-    <div>
-      <Header />
-      <Banner />
-      <main>
-        <ProductList />
-      </main>
-      <Footer />
-    </div>
-  );
-=======
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Banner from "@/components/homePage/Banner";
