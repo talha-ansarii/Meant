f89ec31a5e58a75d12cb331520c175b4edade813
Talha Ansari <talhaansari1606@gmail.com>
2024-08-10 17:34:58 +0530

    final commit8

--- components/homePage/HeroSection.jsx
+++ components/homePage/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
     <div>
       <div className="relative flex justify-center  items-center h-[calc(100vh-100px)] md:h-[800px] lg:h-[800px] bg-black w-full ">
         
-        <div className="absolute font-poppins italiana left-[70%] top-[390px] lg:top-[150px] lg:left-[50%] lg:translate-x-[150px] text-md lg:text-3xl">Limited Edition</div>
+        <div className="absolute font-poppins italiana left-[70%] top-[590px] lg:top-[150px] lg:left-[50%] lg:translate-x-[150px] text-md lg:text-3xl">Limited Edition</div>
        <img
 
        src={"/assets/images/close.png"}
