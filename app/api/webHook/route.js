edba1cf4820deacf449504a568c5565853b8c7b7
Talha Ansari <talhaansari1606@gmail.com>
2024-08-10 14:54:57 +0530

    final commit8

--- app/api/webHook/route.js
+++ app/api/webHook/route.js
@@ -5,41 +5,51 @@ export async function POST(request) {
   await dbConnect();
   console.log("webhook called");
   const body = await request.json();
+
   console.log(body);
   const secret = process.env.SR_WEBHOOK_SECRET;
+  console.log(secret)
   const requestHeaders = new Headers(request.headers);
   const webhookSignature = requestHeaders.get("x-api-key");
-  const { current_status, order_id } = body;
 
-  if (webhookSignature === secret) {
-    try {
-      const updatedOrder = await Orders.findOneAndUpdate(
-        { razorpay_order_id: order_id },
-        { tracking_status: current_status },
-        { new: true } // This option returns the updated document
-      );
+  console.log(webhookSignature)
+  // const { current_status, order_id } = body;
+  // console.log(webhookSignature)
+  // if (webhookSignature === secret) {
+  //   try {
+  //     const updatedOrder = await Orders.findOneAndUpdate(
+  //       { razorpay_order_id: order_id },
+  //       { tracking_status: current_status },
+  //       { new: true } // This option returns the updated document
+  //     );
 
-      if (!updatedOrder) {
-        throw new Error("Order not found");
-      }
+  //     if (!updatedOrder) {
+  //       throw new Error("Order not found");
+  //     }
 
-      console.log(updatedOrder);
+  //     console.log(updatedOrder);
 
-      return new Response(
-        JSON.stringify({
-          message: "Webhook received successfully",
-        }),
-        { status: 200 }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    return new Response(
-      JSON.stringify({
-        message: "Invalid webhook signature",
-      }),
-      { status: 400 }
-    );
-  }
+  //     return new Response(
+  //       JSON.stringify({
+  //         message: "Webhook received successfully",
+  //       }),
+  //       { status: 200 }
+  //     );
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // } else {
+  //   return new Response(
+  //     JSON.stringify({
+  //       message: "Invalid webhook signature",
+  //     }),
+  //     { status: 400 }
+  //   );
+  // }
+  return new Response(
+    JSON.stringify({
+      body
+    }),
+    { status: 200 }
+  );
 }
