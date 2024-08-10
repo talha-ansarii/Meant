23da1d25a7ad419477d9634d3393ae47c56e66bf
Talha Ansari <talhaansari1606@gmail.com>
2024-08-10 13:54:08 +0530

    final commit8

--- components/homePage/Night.jsx
+++ components/homePage/Night.jsx
@@ -220,16 +220,16 @@ const Night = ({ products, setShow, show }) => {
                 <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
               </svg>
 
-              {/* <HoverBorderGradient
+              <HoverBorderGradient
                 containerClassName="rounded-full"
                 as="div"
-                className="font-poppins font-[500] text-[16px] leading-[24px]  w-[100px] h-[30px] bg-black text-white rounded-[34px]"
-                // onClick={handleAddToCart}
-                // disabled={isInCart}
+                className="font-poppins font-[500] text-[10px] leading-[18px]  w-[100px] h-[30px] bg-black text-white rounded-[34px]"
+                onClick={handleAddToCart}
+                disabled={isInCart}
               >
               
                 <span>ADD TO CART</span>
-              </HoverBorderGradient> */}
+              </HoverBorderGradient>
               <HoverBorderGradient
                 containerClassName="rounded-full"
                 as="div"
